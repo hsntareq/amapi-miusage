9f2ea0429b7d352164da423dd42ffa0848236885
Hasan Tareq <hsntareq@gmail.com>
2024-06-18 10:53:42 +0600

    Update bundle code

--- bundle.mjs
+++ bundle.mjs
@@ -1,7 +1,8 @@
 import { execSync } from 'child_process';
 
-const outputFileName = `hasan-miusage.zip`;
-const excludes = `.DS_Store */.DS_Store */*/.DS_Store mix-manifest.json .git .gitattributes .github .editorconfig .gitignore .php-cs-fixer.cache gulpfile.js composer.lock node_modules package-lock.json package.json bundle.mjs composer.json webpack.mix.js src yarn.lock bundle.js phpcs.xml vite.config.mjs hasan-miusage.zip`;
+const fileName = `hasan-miusage`;
+const outputFileName = `${fileName}.zip`;
+const excludes = `.DS_Store */.DS_Store */*/.DS_Store mix-manifest.json .git .gitattributes .github .editorconfig .gitignore .php-cs-fixer.cache gulpfile.js composer.lock node_modules package-lock.json package.json bundle.mjs composer.json webpack.mix.js src yarn.lock bundle.js phpcs.xml *.mjs *.zip ${fileName}`;
 
 const command = `dir-archiver --src . --dest ./${outputFileName} --exclude ${excludes}`;
 try {
